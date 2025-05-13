f58663ed0515386088660e30b63ed6e9415cefb4
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-05-12 22:50:37 -0300

    refactor: rectify path and attach DTO class to the create_payment mock function.

--- backend/PoC/adapters/outbound/mock_payment_repository.py
+++ backend/PoC/adapters/outbound/mock_payment_repository.py
@@ -1,16 +1,14 @@
-from application.ports.order_repository_port import OrderRepositoryPort
 from application.ports.payment_repository_port import PaymentRepositoryPort
-from domain.entities.order import OrderStatus
-from domain.entities.payment import Payment
 from domain.dtos.payment_create import CreatePaymentDTO
+from domain.entities.payment import Payment
 from uuid import uuid4
 from datetime import datetime
 
-class MockPaymentRepository(PaymentRepositoryPort):#CreatePaymentDTO
+class MockPaymentRepository(PaymentRepositoryPort):
     def __init__(self):
         self.payments = {"credit": [], "debit": [], "pix": []}
 
-    def create_payment(self, payment_dto: PaymentCreateDTO) -> Payment:
+    def create_payment(self, payment_dto: CreatePaymentDTO) -> Payment:
         payment = Payment(
             id=uuid4(),
             order_id=payment_dto.order_id,
