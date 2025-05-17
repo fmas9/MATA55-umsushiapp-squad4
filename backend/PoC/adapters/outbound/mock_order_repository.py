78e092e36f61d072b73474e16682495b690bed20
Gabriel Carvalho <gccomputacao@gmail.com>
2025-05-17 18:27:06 -0300

    refactor

--- backend/PoC/adapters/outbound/mock_order_repository.py
+++ backend/PoC/adapters/outbound/mock_order_repository.py
@@ -1,17 +1,17 @@
 from typing import List
-from uuid import UUID
-from domain.entities.order import Order
-from domain.entities.order import OrderStatus
+
 from application.ports.order_repository_port import OrderRepositoryPort
+from domain.entities.order import Order, OrderStatus
+
 
 class MockOrderRepository(OrderRepositoryPort):
     def __init__(self):
         self._orders: List[Order] = []
 
     async def list_orders(self, offset: int = 0, limit: int = 10) -> List[Order]:
-        return self._orders[offset:offset + limit]
-    
-    async def get_order_by_id(self, order_id: UUID):
+        return self._orders[offset : offset + limit]
+
+    async def get_order_by_id(self, order_id: str) -> Order | None:
         for order in self._orders:
             if order.id == order_id:
                 return order
@@ -20,7 +20,7 @@ class MockOrderRepository(OrderRepositoryPort):
     async def save_order(self, order: Order) -> None:
         self._orders.append(order)
 
-    async def checkout_order(self, order_id: UUID):
+    async def checkout_order(self, order_id: str) -> Order | None:
         order = await self.get_order_by_id(order_id)
         if order and order.status == OrderStatus.InProgress:
             order.status = OrderStatus.AwaitingPayment
--- backend/PoC/application/services/payment_service.py
+++ backend/PoC/application/services/payment_service.py
@@ -1,21 +1,25 @@
 from datetime import datetime, timezone
 from typing import List
-from uuid import uuid4
-from domain.dtos.payment_create import CreatePaymentDTO
-from domain.entities.payment import Payment
-from application.ports.payment_repository_port import PaymentRepositoryPort
+from uuid import UUID, uuid4
+
 from application.ports.order_repository_port import OrderRepositoryPort
+from application.ports.payment_repository_port import PaymentRepositoryPort
+from domain.dtos.payment_create import CreatePaymentDTO
 from domain.entities.order import OrderStatus
+from domain.entities.payment import Payment
+
 
 class PaymentService:
-    def __init__(self, repository: PaymentRepositoryPort, order_repository: OrderRepositoryPort):
+    def __init__(
+        self, repository: PaymentRepositoryPort, order_repository: OrderRepositoryPort
+    ):
         self._repo = repository
         self._order_repository = order_repository
 
     async def list_payments(self, page: int = 1, size: int = 10) -> List[Payment]:
         offset = (page - 1) * size
         return await self._repo.list_payments(offset=offset, limit=size)
-    
+
     async def create_payment(self, payment_dto: CreatePaymentDTO) -> Payment:
         payment = Payment(
             id=uuid4(),
@@ -27,16 +31,21 @@ class PaymentService:
         await self._repo.save_payment_history(payment)
         return payment
 
-    async def process_payment(self, order_id: str) -> Payment:
-
+    async def process_payment(
+        self, order_id: str
+    ) -> dict[str, UUID | OrderStatus] | None:
         order = await self._order_repository.get_order_by_id(order_id)
-        #print("D\Exceção aqui {}".format(order))
+        # print("D\Exceção aqui {}".format(order))
 
-        if order.status != OrderStatus.AwaitingPayment:
-            raise ValueError("Pedido em estado inválido.")
-        # Pix gateway call TODO: Implement pix gateway
-        # resp = self.pix_gateway.create_charge(amount=order.amount, external_id=order.id)
+        if order is not None:
+            if order.status != OrderStatus.AwaitingPayment:
+                raise ValueError("Pedido em estado inválido.")
+            # Pix gateway call TODO: Implement pix gateway
+            # resp = self.pix_gateway.create_charge(amount=order.amount, external_id=order.id)
 
-        order.status = OrderStatus.PaymentCompleted
-        await self._order_repository.save(order)
-        return {"order_id": order.id, "new_status": order.status} #{"order_id": order.id, "new_status": order.status, "pix_key": resp["pix_key"]}
+            order.status = OrderStatus.PaymentCompleted
+            await self._order_repository.save_order(order)
+            return {
+                "order_id": order.id,
+                "new_status": order.status,
+            }  # {"order_id": order.id, "new_status": order.status, "pix_key": resp["pix_key"]}
