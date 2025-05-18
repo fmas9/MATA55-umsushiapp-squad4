503d85e617f1f9f140017e91ca570b890d2c5007
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-05-18 01:17:37 -0300

    refactor: remover order_status e transferindo para pasta de enums(arquivo de responsabilidade unica).

--- backend/PoC/domain/entities/order.py
+++ backend/PoC/domain/entities/order.py
@@ -1,15 +1,10 @@
 from dataclasses import dataclass
 from enum import Enum
-from uuid import UUID, uuid4
+from uuid import UUID
 from datetime import datetime
-from typing import List, Any
+from typing import List
 from pydantic import BaseModel
-
-class OrderStatus(Enum):
-    InProgress = "InProgress"
-    AwaitingPayment = "AwaitingPayment"
-    PaymentCompleted = "PaymentCompleted"
-    Completed = "Completed"
+from enums.order_type import OrderStatus
 
 class Order(BaseModel):
     id: UUID
