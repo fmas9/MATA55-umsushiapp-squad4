a9df1f40fcb28df993e5705f5576095257697e07
Gabriel Carvalho <gccomputacao@gmail.com>
2025-05-17 22:24:36 -0300

    test: ajustar test_routes.py

--- backend/PoC/tests/test_routes.py
+++ backend/PoC/tests/test_routes.py
@@ -7,56 +7,77 @@ port = "8000"
 
 url = f"{base}:{port}"
 
-print(
+
+def tester(message: str):
+    def decorator(f):
+        def wrapper(*args):
+            print(message)
+            result = f(*args)
+            input("Aperte <Enter>\n")
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@tester(
     "Este programa testa automaticamente as rotas '/orders' e '/payments', enviando requisições POST e GET, para criar e ler os recursos do sistema.\n"
 )
+def init():
+    pass
 
-input("Aperte <Enter>\n")
 
-# GET na rota /orders
-print(
+@tester(
     f"Enviando requisição GET na url: {url}/orders/, para listar os pedidos salvos.\n"
 )
-res = requests.get(f"{url}/orders")
+def test_list_orders():
+    res = requests.get(f"{url}/orders")
 
-if res.status_code == 200 and type(res.json()) is list:
-    print(f"status code: {res.status_code}\n", f"conteudo: {res.json()}\n")
+    if res.status_code == 200 and type(res.json()) is list:
+        print(f"status code: {res.status_code}\n", f"conteudo: {res.json()}\n")
 
-input("Aperte <Enter>\n")
 
-# POST na rota /orders
-new_order = {"items": ["Acarajé", "Coca-Cola"], "total": 15.50}
+@tester(f"Enviando requisição POST na url: {url}/orders/, para criar um novo pedido.\n")
+def test_create_order():
+    new_order = {"items": ["Acarajé", "Coca-Cola"], "total": 15.50}
 
-print(f"Enviando requisição POST na url: {url}/orders/, para criar um novo pedido.\n")
-res = requests.post(f"{url}/orders", json.dumps(new_order))
+    res = requests.post(f"{url}/orders", json.dumps(new_order))
 
-if res.status_code == 201:
-    id = res.json()["id"]
-    print(f"status code: {res.status_code}\n", f"id do novo pedido: {id}\n")
+    if res.status_code == 201:
+        order_id = res.json()["id"]
+        print(f"status code: {res.status_code}\n", f"id do novo pedido: {order_id}\n")
 
-input("Aperte <Enter>\n")
+        return order_id
 
-# POST na rota /payments
-new_payment = {"order_id": id, "amount": new_order["total"], "payment_type": "pix"}
 
-print(
+@tester(
     f"Enviando requisição POST na url: {url}/payments/, para criar um novo pagamento com o id obtido anteriomente.\n"
 )
-res = requests.post(f"{url}/payments", json.dumps(new_payment))
+def test_create_payment(order_id):
+    new_payment = {"order_id": order_id, "amount": 15.50, "payment_type": "pix"}
+    res = requests.post(f"{url}/payments", json.dumps(new_payment))
 
-if res.status_code == 201:
-    id = res.json()["id"]
-    print(f"status code: {res.status_code}\n", f"id do novo pagamento: {id}\n")
+    if res.status_code == 201:
+        payment_id = res.json()["id"]
+        print(
+            f"status code: {res.status_code}\n", f"id do novo pagamento: {payment_id}\n"
+        )
 
-input("Aperte <Enter>\n")
 
-# GET na rota /payments
-print(
+@tester(
     f"Enviando requisição GET na url: {url}/payments/, para listar os pagamentos salvos.\n"
 )
-res = requests.get(f"{url}/payments")
+def test_list_payments():
+    res = requests.get(f"{url}/payments")
 
-if res.status_code == 200 and type(res.json()) is list:
-    print(f"status code: {res.status_code}\n", f"conteudo: {res.json()}\n")
+    if res.status_code == 200 and type(res.json()) is list:
+        print(f"status code: {res.status_code}\n", f"conteudo: {res.json()}\n")
 
-input("Aperte <Enter>\n")
+
+init()
+test_list_orders()
+order_id = test_create_order()
+test_create_payment(order_id)
+test_list_payments()
