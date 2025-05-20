31b47a51d8e6ff84ca763f6f47c4f5f136aea680
Jimmy Starling <jimmystarlingdev@gmail.com>
2025-05-19 23:59:55 -0300

    feat: add transaction

--- backend/PoC/domain/entities/transaction.py
+++ backend/PoC/domain/entities/transaction.py
@@ -1,14 +1,19 @@
+from typing import List
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
 from domain.entities.order import OrderStatus
+from domain.entities.product import Product
 from enums.payment_type import PaymentType
 
 class Transaction(BaseModel):
     id: UUID
-    order_id: UUID
-    order_status: OrderStatus
-    payment_id: UUID
-    payment_type: PaymentType
-    payment_date: datetime
-    payment_total: float
+    #order_id: UUID
+    #order_status: OrderStatus
+    items: List[Product] # MercadoPago
+    # payment_id: UUID
+    # payment_type: PaymentType
+    # payment_date: datetime
+    # payment_total: float
+    back_urls: List[str] # MercadoPago
+    auto_return: "all" # MercadoPago
