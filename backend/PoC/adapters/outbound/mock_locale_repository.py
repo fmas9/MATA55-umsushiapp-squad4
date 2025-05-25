99840f77273935679f252025d2d34e9a7025484e
VictorAlexaM <alexandrevictor566@gmail.com>
2025-05-25 19:59:35 -0300

    refactor: mudança do nome da variavel para locale

--- backend/PoC/adapters/outbound/mock_locale_repository.py
+++ backend/PoC/adapters/outbound/mock_locale_repository.py
@@ -1,16 +1,16 @@
-from backend.PoC.domain.entities.locale import Regiao
-regioes = []
+from backend.PoC.domain.entities.locale import Locale
+locales = []
 
 def cadastrar_regiao(): #cadastro pelo gerente
-    nome = input("Nome da região: ")
-    regiao = Regiao(nome)
-    regioes.append(regiao)
-    print(f"✅ Região '{nome}' cadastrada com sucesso!")
+    name = input("Nome da região: ")
+    locale = Locale(name)
+    locales.append(locale)
+    print(f"✅ Região '{name}' cadastrada com sucesso!")
 
 def listar_regioes():
-    if not regioes:
+    if not locales:
         print(" Nenhuma região cadastrada.")
     else:
         print("Regiões cadastradas:")
-        for i, r in enumerate(regioes, start=1):
+        for i, r in enumerate(locales, start=1):
             print(f"{i}. {r.nome}")
