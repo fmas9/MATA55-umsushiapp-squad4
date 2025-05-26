f1b39c632061c66a64245f6a87041e92248feeff
VictorAlexaM <alexandrevictor566@gmail.com>
2025-05-25 21:16:03 -0300

    refactor: alterando def cadastra e listar

--- backend/PoC/adapters/outbound/mock_locale_repository.py
+++ backend/PoC/adapters/outbound/mock_locale_repository.py
@@ -1,16 +1,24 @@
 from backend.PoC.domain.entities.locale import Locale
+from typing import List
+from uuid import UUID
+from locale import locale  # sua entidade
+
 locales = []
-
-def cadastrar_regiao(): #cadastro pelo gerente
-    name = input("Nome da região: ")
-    locale = Locale(name)
-    locales.append(locale)
-    print(f"✅ Região '{name}' cadastrada com sucesso!")
-
-def listar_regioes():
-    if not locales:
-        print(" Nenhuma região cadastrada.")
-    else:
-        print("Regiões cadastradas:")
-        for i, r in enumerate(locales, start=1):
-            print(f"{i}. {r.nome}")
+class LocaleMockRepository:
+    def __init__(self):
+        self._locale: List[locale] = []
+        return None
+    
+    async def get_locale_by_id(self, locale_id: UUID) -> Locale | None:
+        for locale in self._locales:
+            if locale.id == locale_id:
+                return locale
+            return None
+    async def save_locale(self, locale: locale) -> Locale | None:
+        for idx, existing_locale in enumerate(self._locale):
+            if existing_locale.id == locale.id:
+                self._locale[idx] = locale  # Atualiza
+                return
+        self._locale.append(locale)  # Adiciona nova região
+    async def listar_locale(self) -> List[locale]:
+        return self._locale
